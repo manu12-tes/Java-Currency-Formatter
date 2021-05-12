bf73b0958e121c5655e555b2c4950182711fd55e
Manuel Garcia <kiramanuel123@gmail.com>
2021-05-12 02:21:37 -0500

    Agregar archivos de proyecto.

---
+++ Java-Currency-Formatter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Java_Currency_Formatter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dame un valor");
+            double pago = Convert.ToDouble(Console.ReadLine());
+
+            if (pago>0 && pago < (10 ^ 9))
+            {
+                Console.WriteLine("Us:"+"$"+pago);
+                Console.WriteLine("India:"+"Rs."+pago);
+                Console.WriteLine("China:"+ "￥"+ pago);
+                Console.WriteLine("France:" + pago + "€");
+            }
+            else
+            {
+                Console.WriteLine("no se puede dar conversion");
+            }
+        }
+    }
+}
